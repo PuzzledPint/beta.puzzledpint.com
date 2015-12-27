0b93ac1d273add0be28182033e69cec810190913
Victor Asteinza <vic+github@civrot.org>
2015-12-26 16:18:25 -0800

    Add validations for EventLocation

--- app/models/event_location.rb
+++ app/models/event_location.rb
@@ -1,4 +1,7 @@
 class EventLocation < ActiveRecord::Base
   belongs_to :city
   belongs_to :event
+
+  validates :city, presence: true
+  validates :event, presence: true
 end
--- spec/models/event_location_spec.rb
+++ spec/models/event_location_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe EventLocation, type: :model do
   it { should belong_to :city }
   it { should belong_to :event }
+  it { should validate_presence_of :city }
+  it { should validate_presence_of :event }
 end
