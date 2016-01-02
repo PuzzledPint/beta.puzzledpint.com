af0188462ac251ed61a010961067062e52753c33
Victor Asteinza <vic+github@civrot.org>
2016-01-02 13:07:49 -0800

    Clean up some date time displays

--- config/initializers/date_time.rb
+++ config/initializers/date_time.rb
@@ -1,3 +1,4 @@
 Date::DATE_FORMATS[:default] = '%m/%d/%Y %I:%M %p'
+Date::DATE_FORMATS[:date] = '%m/%d/%Y'
 Time::DATE_FORMATS[:default] = '%m/%d/%Y %I:%M %p'
 Time::DATE_FORMATS[:time] = '%I:%M %P'
