d36b5afbc9a8ababfbe7b1bfd8982ccaa200c5b5
Victor Asteinza <vic+github@civrot.org>
2016-01-01 17:16:46 -0800

    Add access origin allow

--- app/controllers/event_locations_controller.rb
+++ app/controllers/event_locations_controller.rb
@@ -1,5 +1,7 @@
 class EventLocationsController < ApplicationController
   def index
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
     event = Event.find(params[:event_id])
     @locations = event.locations_tree
   end
