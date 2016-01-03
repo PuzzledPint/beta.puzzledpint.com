ba6d1895870b6256d7914924897d005b8e5d0632
Victor Asteinza <vic+github@civrot.org>
2016-01-02 20:34:25 -0800

    Include wysihtml5 later in include list

--- app/assets/javascripts/game_control_footer.js
+++ app/assets/javascripts/game_control_footer.js
@@ -3,6 +3,6 @@
 //= require AdminLTE/plugins/datatables/dataTables.bootstrap.min.js
 //= require AdminLTE/plugins/select2/select2.min.js
 //= require country_state_select
-//= require game_control/bootstrap3-wysihtml5.all.min.js
 //= require AdminLTE/plugins/iCheck/icheck.min.js
 //= require AdminLTE/dist/js/app.min.js
+//= require game_control/bootstrap3-wysihtml5.all.min.js
