17f506fcfec84377d1be29d6f764d12a9f40a81a
Victor Asteinza <vic+github@civrot.org>
2016-01-02 19:39:21 -0800

    Fix jquery and databales issue.

--- app/assets/javascripts/game_control_footer.js
+++ app/assets/javascripts/game_control_footer.js
@@ -1,9 +1,8 @@
-//= require AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js
 //= require AdminLTE/bootstrap/js/bootstrap.min.js
-//= require AdminLTE/plugins/iCheck/icheck.min.js
 //= require AdminLTE/plugins/datatables/jquery.dataTables.min.js
 //= require AdminLTE/plugins/datatables/dataTables.bootstrap.min.js
 //= require AdminLTE/plugins/select2/select2.min.js
 //= require country_state_select
 //= require game_control/bootstrap3-wysihtml5.all.min.js
+//= require AdminLTE/plugins/iCheck/icheck.min.js
 //= require AdminLTE/dist/js/app.min.js
