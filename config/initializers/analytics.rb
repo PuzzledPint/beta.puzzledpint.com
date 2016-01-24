050a906187d566db94521eefec8057cc3eba1ea4
Victor Asteinza <vic+github@civrot.org>
2016-01-24 15:04:18 -0800

    Only enable segment.io in production mode

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,4 +1,10 @@
 require 'segment/analytics'
 
-Analytics = Segment::Analytics.new(write_key: ENV['SEGMENT_IO_KEY'],
-                                   on_error: Proc.new { |_, msg| Rails.logger.error(msg) })
+opts = {
+  write_key: ENV['SEGMENT_IO_KEY'],
+  on_error: Proc.new { |_, msg| Rails.logger.error(msg) }
+}
+
+opts.merge(stub: true) unless Rails.env.production?
+
+Analytics = Segment::Analytics.new(opts)
