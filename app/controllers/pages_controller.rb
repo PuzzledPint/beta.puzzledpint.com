93172d6a47fb1c338ddb4003c26efe2c65477205
Victor Asteinza <vic+github@civrot.org>
2016-04-07 23:25:40 -0700

    Add page preview

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def show
-    @page = Page.where(active: true, slug: params[:slug]).first
+    @page = Page.where(slug: params[:slug])
+    @page = @page.where(active: true) unless params[:preview].present?
+    @page = @page.first
     render 'errors/404', status: :not_found unless @page.present?
   end
 end
