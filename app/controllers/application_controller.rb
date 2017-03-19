347a237050fa1437268556b9d92082f42b3e2219
Victor Asteinza <vic+github@civrot.org>
2017-03-19 16:10:48 -0700

    Route user correctly based on sign_in status
    Fixes #70
    
    Added GameControl::SessionsController to overwrite the default paths
    after sign in and sign out.
    
    Removed previous fix that fixed half the problem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  after_filter :store_action
-
-  private
-
-  def after_sign_out_path_for(_)
-    game_control_root_path
-  end
-
-
-  def store_action
-    return unless request.get?
-    if (request.path.end_with?("/sign_in") &&
-        !request.xhr?) # don't store ajax calls
-      store_location_for(:admin, game_control_root_path)
-    end
-  end
 end
